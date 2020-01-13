c54f9de3d4d5c670672f425efebebe7b49aa4cc0
nkiv <no.kivett@gmail.com>
2020-01-12 21:16:46 -0700

    Worked on challenge one

---
+++ DogMain.java
@@ -0,0 +1,8 @@
+public class DogMain{
+
+    public static void main(String [] args){
+        Dog dog1 = new Dog(27.5, alfred, 5, true);
+        Dog dog2 = new Dog(78.5, DocOck, 10, false);
+
+    }
+}
---
+++ DogMethod.java
@@ -0,0 +1,15 @@
+public class DogMethod{
+
+    private double weight;
+    private String dogName;
+    private int dogAge;
+    private boolean tailWags;
+
+    public void Dog(double weight, String dogName, int dogAge, boolean tailwags){
+        this.weight = weight;
+        this.dogName = dogName;
+        this.dogAge = dogAge;
+        this.tailWags = tailWags;
+
+    }
+}
